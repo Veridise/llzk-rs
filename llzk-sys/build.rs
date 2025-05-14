22b7650d8f9a8e9ccf76b35ad985799b25a463d3
Daniel Dominguez <daniel@veridise.com>
2025-05-14 17:50:30 +0200

    Add generated bindings and sanity tests to check if linking is working correctly

--- llzk-sys/build.rs
+++ llzk-sys/build.rs
@@ -1,11 +1,12 @@
 use cmake::Config;
 use glob::glob;
 use std::{
+    collections::HashSet,
     env,
     error::Error,
     ffi::OsStr,
     fs::read_dir,
-    path::Path,
+    path::{Path, PathBuf},
     process::{exit, Command},
     str,
 };
@@ -18,30 +19,57 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
+    let mlir_path = env::var("DEP_MLIR_PREFIX")?;
+    let llzk_src_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("llzk-lib");
     let llzk_path = Config::new("llzk-lib")
         .define("LLZK_BUILD_DEVTOOLS", "OFF")
         .define("BUILD_TESTING", "OFF")
-        .define("LLVM_DIR", env::var("DEP_MLIR_PREFIX")?)
-        .define("MLIR_DIR", env::var("DEP_MLIR_PREFIX")?)
-        //.build_target("LLZKDialect")
-        //.build_target("LLZKDialectRegistration")
+        .define("LLVM_DIR", &mlir_path)
+        .define("MLIR_DIR", &mlir_path)
+        .build_target("LLZKDialectRegistration")
+        .build_target("LLZKTransforms")
+        .build_target("LLZKValidators")
         .build();
     eprintln!("llzk_path = {}", llzk_path.display());
-    println!("cargo:rustc-link-search={}/build/lib", llzk_path.display());
+    eprintln!("llzk_src_path = {}", llzk_src_path.display());
 
+    let mut seen: HashSet<PathBuf> = Default::default();
     for f in glob(llzk_path.join("**/*.a").to_str().unwrap())? {
         let f = f?;
-        eprintln!("=== {}", f.display());
+        if let Some(parent) = f.parent().and_then(|p| Some(p.to_path_buf())) {
+            if !seen.contains(&parent) {
+                println!("cargo:rustc-link-search={}", parent.display());
+                seen.insert(parent);
+            }
+        }
+        if let Some(name) = f
+            .file_name()
+            .and_then(OsStr::to_str)
+            .and_then(parse_archive_name)
+        {
+            println!("cargo:rustc-link-lib={}", name);
+        }
     }
-    println!("cargo:rustc-link-lib=LLZKDialect");
-    println!("cargo:rustc-link-lib=LLZKDialectRegistration");
 
     bindgen::builder()
         .header("wrapper.h")
+        .clang_arg(format!("-I{}", llzk_src_path.join("include").display()))
         .clang_arg(format!("-I{}", llzk_path.join("build/include").display()))
+        .clang_arg(format!(
+            "-I{}",
+            Path::new(&mlir_path).join("include").display()
+        ))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()?
         .write_to_file(Path::new(&env::var("OUT_DIR")?).join("bindings.rs"))?;
 
     Ok(())
+}
+
+fn parse_archive_name(name: &str) -> Option<&str> {
+    if let Some(name) = name.strip_prefix("lib") {
+        name.strip_suffix(".a")
+    } else {
+        None
+    }
 }
--- mlir-18-sys/src/lib.rs
+++ mlir-18-sys/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
-mod tests {
+mod sanity_tests {
     use super::*;
+    use std::ffi::CString;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_context() {
+        unsafe {
+            let ctx = mlirContextCreate();
+            assert!(mlirContextEqual(ctx, ctx));
+            mlirContextDestroy(ctx);
+        }
+    }
+
+    #[test]
+    fn create_string() {
+        unsafe {
+            let string = CString::new("Hello, world!").unwrap();
+
+            mlirStringRefCreateFromCString(string.as_ptr());
+        }
+    }
+
+    #[test]
+    fn test_location() {
+        unsafe {
+            let registry = mlirDialectRegistryCreate();
+            let context = mlirContextCreate();
+
+            mlirContextAppendDialectRegistry(context, registry);
+            mlirRegisterAllDialects(registry);
+
+            let location = mlirLocationUnknownGet(context);
+            let string = CString::new("newmod").unwrap();
+            let reference = mlirStringRefCreateFromCString(string.as_ptr());
+
+            mlirOperationStateGet(reference, location);
+
+            mlirContextDestroy(context);
+            mlirDialectRegistryDestroy(registry);
+        }
     }
 }
