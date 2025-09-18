72fb0c2f90812bf6310e359347aadc2ef387108f
Daniel Dominguez <daniel@veridise.com>
2025-09-18 10:36:30 +0200

    Add complementary canonicalization patterns

--- frontends/halo2/src/ir/canon.rs
+++ frontends/halo2/src/ir/canon.rs
@@ -8,23 +8,29 @@ pub fn canonicalize_constraint(
     rhs: &IRAexpr,
 ) -> Option<(CmpOp, IRAexpr, IRAexpr)> {
     match (op, lhs, rhs) {
-        // (= (+ X (- Y)) 0) => (= X Y)
-        (CmpOp::Eq, IRAexpr::Sum(x, sum_rhs), IRAexpr::Constant(zero)) if *zero == 0usize => {
-            match &**sum_rhs {
-                IRAexpr::Negated(y) => Some((CmpOp::Eq, (**x).clone(), (**y).clone())),
-                _ => None,
+        // (= (+ X (- Y)) 0) => (= X Y) OR (= (+ (- X) Y) 0) => (= X Y)
+        (CmpOp::Eq, IRAexpr::Sum(sum_lhs, sum_rhs), IRAexpr::Constant(zero)) if *zero == 0usize => {
+            if let IRAexpr::Negated(y) = &**sum_rhs {
+                return Some((CmpOp::Eq, (**sum_lhs).clone(), (**y).clone()));
             }
+            if let IRAexpr::Negated(y) = &**sum_lhs {
+                return Some((CmpOp::Eq, (**y).clone(), (**sum_rhs).clone()));
+            }
+            None
         }
-        // (= (* 1 (+ X (- Y))) 0) => (= X Y)
+        // (= (* 1 (+ X (- Y))) 0) => (= X Y) OR (= (* 1 (+ (- X) Y)) 0) => (= X Y)
         (CmpOp::Eq, IRAexpr::Product(mul_lhs, mul_rhs), IRAexpr::Constant(zero))
             if *zero == 0usize =>
         {
             match (&**mul_lhs, &**mul_rhs) {
-                (IRAexpr::Constant(one), IRAexpr::Sum(x, sum_rhs)) if *one == 1usize => {
-                    match &**sum_rhs {
-                        IRAexpr::Negated(y) => Some((CmpOp::Eq, (**x).clone(), (**y).clone())),
-                        _ => None,
+                (IRAexpr::Constant(one), IRAexpr::Sum(sum_lhs, sum_rhs)) if *one == 1usize => {
+                    if let IRAexpr::Negated(y) = &**sum_rhs {
+                        return Some((CmpOp::Eq, (**sum_lhs).clone(), (**y).clone()));
                     }
+                    if let IRAexpr::Negated(y) = &**sum_lhs {
+                        return Some((CmpOp::Eq, (**y).clone(), (**sum_rhs).clone()));
+                    }
+                    None
                 }
                 _ => None,
             }
