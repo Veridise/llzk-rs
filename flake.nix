6f0844ffebb7108413a9a39494de463178046f51
Tim Hoffman <timothy.hoffman@veridise.com>
2025-10-02 16:46:40 -0500

    revert partially: it fixed the z3 issue but not LLVM lib

--- flake.nix
+++ flake.nix
@@ -149,7 +149,7 @@
             MLIR_SYS_200_PREFIX = "${mlir-with-llvm}";
             TABLEGEN_200_PREFIX = "${mlir-with-llvm}";
             LIBCLANG_PATH = "${pkgs.llzk-llvmPackages.libclang.lib}/lib";
-            RUSTFLAGS = "-L ${mlir-with-llvm}/lib";
+            RUSTFLAGS = "-lLLVM -L ${mlir-with-llvm}/lib";
             RUST_BACKTRACE = 1;
             # Fix _FORTIFY_SOURCE warning on Linux by ensuring build dependencies are optimized
             CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_OPT_LEVEL = 2;
