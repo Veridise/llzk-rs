5f5b84a4ab06bbf912165182903b6bde10e1e48f
Daniel Dominguez <daniel@veridise.com>
2025-10-16 17:18:14 +0200

    Fix asan script on macos

--- scripts/test_with_asan.sh
+++ scripts/test_with_asan.sh
@@ -1,9 +1,12 @@
 #!/bin/sh 
 
-RUSTFLAGS="$RUSTFLAGS -Zsanitizer=address" \
-  ASAN_OPTIONS="debug=true:detect_leaks=1:symbolize=1" \
-  ASAN_SYMBOLIZER_PATH="$MLIR_SYS_200_PREFIX/llvm-symbolizer" \
-  CFLAGS="-fsanitize=address" \
-  CXXFLAGS="-fsanitize=address" \
-  LDFLAGS="-fsanitize=address -static-libasan" \
-  cargo +nightly test --target=aarch64-apple-darwin $@
+export RUSTFLAGS="$RUSTFLAGS -Zsanitizer=address"
+
+if [ $(uname) == "Darwin" ]; then 
+  export RUSTFLAGS="$RUSTFLAGS -Zexternal-clangrt -L $MLIR_SYS_200_PREFIX/lib/clang/20/lib/darwin/ -l clang_rt.asan_osx_dynamic"
+fi
+ 
+export ASAN_OPTIONS="debug=true:detect_leaks=1:symbolize=1"
+export ASAN_SYMBOLIZER_PATH="$MLIR_SYS_200_PREFIX/llvm-symbolizer"
+
+cargo +nightly test --target=aarch64-apple-darwin $@
