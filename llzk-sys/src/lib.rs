d9d72b175c1a223cf56c9cb30677bd5e457bebd5
Daniel Dominguez <daniel@veridise.com>
2025-10-16 21:34:31 +0200

    Add comments to lib.rs

--- llzk-sys/src/lib.rs
+++ llzk-sys/src/lib.rs
@@ -1,8 +1,14 @@
+//! Generated bindings of LLZK's C API.
+//!
+//! Follows a similar model to `mlir-sys` and integrates with that crate.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![warn(rustdoc::broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
+// This lint should never set to deny since it depends on code written in the llzk-lib repository.
+// Its set to warn as a reminder for the CAPI maintainers to add missing documenting.
 #![warn(missing_docs)]
 
 use mlir_sys::{
