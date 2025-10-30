45db0b418cc4d02170740f9926d12390ba1c3d70
foldr <daniel@veridise.com>
2025-10-30 17:10:18 +0100

    Document benchmark file (#44)
    * Document benchmark file
    
    * Update picus/benches/vars_benchmark.rs
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
    
    * Update picus/benches/vars_benchmark.rs
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
    
    * Update picus/benches/vars_benchmark.rs
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
    
    * Update picus/benches/vars_benchmark.rs
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>

--- picus/benches/vars_benchmark.rs
+++ picus/benches/vars_benchmark.rs
@@ -5,6 +5,10 @@ use picus::{
 };
 use std::hint::black_box;
 
+/// A key similar to the one used by the Halo2 frontend.
+///
+/// Hashing this struct is not as intense as hashing a string but is not as simple as hashing a
+/// single machine word.
 #[derive(Hash, Eq, PartialEq, PartialOrd, Ord, Debug, Copy, Clone, Default)]
 struct Key(usize, usize, usize);
 
@@ -36,11 +40,35 @@ impl From<Key> for VarStr {
     }
 }
 
+/// Benchmark for different insertion scenarios in the [`Vars`] environment.
+///
+/// Tests element by element insertion in the following scenarios:
+///  1. Direct insertion on the environment without checking for name collisions. Serves as a baseline
+///  of directly inserting on the underlying data structures.
+///  2. Repeatedly inserting the same element, triggering cache hits.
+///  3. Inserting different elements each time. Should be similar to the direct insertion plus a
+///  check for uniqueness.
+///  4. Inserting different elements that result in the same string representation. Forces the
+///  uniquer to check several times for unique names.
+///
+/// For each scenario, we progressively test with a larger amount of inputs: 1, 5, 10, 50, 100.
+/// This gives a decent enough sample size for checking performance and completing the benchmark in
+/// a decent time even if it's under the usual workload seen on production.
+///
+/// The most common scenarios are #2 and #3 so those are the ones that would need targeted
+/// optimizations, if needed.
+///
+/// Note: The calls to [`black_box`] are for hinting the optimizer to leave the argument value alone.
+/// Helps with avoiding the optimizer fudging with the code and screwing the results.
 pub fn insert(c: &mut Criterion) {
+    // We group the different tests together in a group to make the bechmarking framework show
+    // their statistics together.
     let mut group = c.benchmark_group("insert");
 
     for n in [1, 5, 10, 50, 100] {
         group.throughput(Throughput::Elements(n));
+
+        // Direct insertion without checking for name collisions
         group.bench_with_input(BenchmarkId::new("direct", n), &(n as usize), |b, i| {
             b.iter(|| {
                 let mut vars = Vars::<Key>::default();
@@ -52,6 +80,8 @@ pub fn insert(c: &mut Criterion) {
                 }
             })
         });
+
+        // Repeatedly insert the same element, triggering cache hits
         group.bench_with_input(BenchmarkId::new("equal", n), &(n as usize), |b, i| {
             b.iter(|| {
                 let mut vars = Vars::<Key>::default();
@@ -60,6 +90,8 @@ pub fn insert(c: &mut Criterion) {
                 }
             })
         });
+
+        // Insert different elements each time
         group.bench_with_input(BenchmarkId::new("different", n), &(n as usize), |b, i| {
             b.iter(|| {
                 let mut vars = Vars::<Key>::default();
@@ -69,6 +101,7 @@ pub fn insert(c: &mut Criterion) {
             })
         });
 
+        // Insert different elements that result in the same string representation
         group.bench_with_input(
             BenchmarkId::new("different same string", n),
             &(n as usize),
