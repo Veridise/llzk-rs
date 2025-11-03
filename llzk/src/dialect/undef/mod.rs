283180e5e6af6c96e560f85e8a83d93ab86eae0c
foldr <daniel@veridise.com>
2025-11-03 20:12:41 +0100

    Add rust version of isa function (#51)
    * Add rust version of isa function
    
    * Forgot to push submodule update

--- llzk/src/dialect/undef/mod.rs
+++ llzk/src/dialect/undef/mod.rs
@@ -1,7 +1,7 @@
-use llzk_sys::mlirGetDialectHandle__llzk__undef__;
+use llzk_sys::{llzkOperationIsAUndefOp, mlirGetDialectHandle__llzk__undef__};
 use melior::{
     dialect::DialectHandle,
-    ir::{Location, Operation, Type, operation::OperationBuilder},
+    ir::{Location, Operation, OperationRef, Type, operation::OperationBuilder},
 };
 
 pub fn handle() -> DialectHandle {
@@ -15,4 +15,9 @@ pub fn undef<'c>(location: Location<'c>, result: Type<'c>) -> Operation<'c> {
         .add_results(&[result])
         .build()
         .expect("valid operation")
+}
+
+/// Returns wether the given operation is a 'undef.undef' operation or not.
+pub fn is_undef_op(op: OperationRef) -> bool {
+    unsafe { llzkOperationIsAUndefOp(op.to_raw()) }
 }
