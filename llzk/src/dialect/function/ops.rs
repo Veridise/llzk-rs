c0f54b08fe1986afa46ae43206fd29cd9ee15873
Tim Hoffman <timothy.hoffman@veridise.com>
2025-12-03 21:29:24 -0600

    support additional functions on FuncDef and Call ops

--- llzk/src/dialect/function/ops.rs
+++ llzk/src/dialect/function/ops.rs
@@ -8,11 +8,15 @@ use crate::{
 };
 
 use llzk_sys::{
-    llzkCallOpBuild, llzkFuncDefOpCreateWithAttrsAndArgAttrs, llzkFuncDefOpGetFullyQualifiedName,
+    llzkCallOpBuild, llzkCallOpGetCalleeIsCompute, llzkCallOpGetCalleeIsConstrain,
+    llzkCallOpGetCalleeIsStructCompute, llzkCallOpGetCalleeIsStructConstrain,
+    llzkCallOpGetSelfValueFromCompute, llzkCallOpGetSelfValueFromConstrain,
+    llzkFuncDefOpCreateWithAttrsAndArgAttrs, llzkFuncDefOpGetFullyQualifiedName,
     llzkFuncDefOpGetHasAllowConstraintAttr, llzkFuncDefOpGetHasAllowWitnessAttr,
     llzkFuncDefOpGetHasArgIsPub, llzkFuncDefOpGetIsInStruct, llzkFuncDefOpGetIsStructCompute,
     llzkFuncDefOpGetIsStructConstrain, llzkFuncDefOpGetNameIsCompute,
-    llzkFuncDefOpGetNameIsConstrain, llzkFuncDefOpGetSingleResultTypeOfCompute,
+    llzkFuncDefOpGetNameIsConstrain, llzkFuncDefOpGetSelfValueFromCompute,
+    llzkFuncDefOpGetSelfValueFromConstrain, llzkFuncDefOpGetSingleResultTypeOfCompute,
     llzkFuncDefOpSetAllowConstraintAttr, llzkFuncDefOpSetAllowWitnessAttr, llzkOperationIsACallOp,
     llzkOperationIsAFuncDefOp,
 };
@@ -81,12 +85,12 @@ pub trait FuncDefOpLike<'c: 'a, 'a>: OperationLike<'c, 'a> {
         unsafe { Attribute::from_raw(llzkFuncDefOpGetFullyQualifiedName(self.to_raw())) }
     }
 
-    /// Returns true if the function's name is 'compute'.
+    /// Returns true if the function's name is `LLZK_FUNC_NAME_COMPUTE`.
     fn name_is_compute(&self) -> bool {
         unsafe { llzkFuncDefOpGetNameIsCompute(self.to_raw()) }
     }
 
-    /// Returns true if the function's name is 'constrain'.
+    /// Returns true if the function's name is `LLZK_FUNC_NAME_CONSTRAIN`.
     fn name_is_constrain(&self) -> bool {
         unsafe { llzkFuncDefOpGetNameIsConstrain(self.to_raw()) }
     }
@@ -104,6 +108,18 @@ pub trait FuncDefOpLike<'c: 'a, 'a>: OperationLike<'c, 'a> {
     /// Returns true if the function is the struct's constrain definition.
     fn is_struct_constrain(&self) -> bool {
         unsafe { llzkFuncDefOpGetIsStructConstrain(self.to_raw()) }
+    }
+
+    /// Return the "self" value (i.e. the return value) from the function (which
+    /// must be named `LLZK_FUNC_NAME_COMPUTE`).
+    fn self_value_of_compute(&self) -> Value<'c, 'a> {
+        unsafe { Value::from_raw(llzkFuncDefOpGetSelfValueFromCompute(self.to_raw())) }
+    }
+
+    /// Return the "self" value (i.e. the first parameter) from the function (which
+    /// must be named `LLZK_FUNC_NAME_CONSTRAIN`).
+    fn self_value_of_constrain(&self) -> Value<'c, 'a> {
+        unsafe { Value::from_raw(llzkFuncDefOpGetSelfValueFromConstrain(self.to_raw())) }
     }
 
     /// Assuming the function is the compute function returns its StructType result.
@@ -164,7 +180,39 @@ impl<'a, 'c: 'a> FuncDefOpMutLike<'c, 'a> for FuncDefOpRefMut<'c, 'a> {}
 //===----------------------------------------------------------------------===//
 
 /// Defines the public API of the 'function.call' op.
-pub trait CallOpLike<'c: 'a, 'a>: OperationLike<'c, 'a> {}
+pub trait CallOpLike<'c: 'a, 'a>: OperationLike<'c, 'a> {
+    /// Returns true if the call target name is `LLZK_FUNC_NAME_COMPUTE`.
+    fn callee_is_compute(&self) -> bool {
+        unsafe { llzkCallOpGetCalleeIsCompute(self.to_raw()) }
+    }
+
+    /// Returns true if the call target name is `LLZK_FUNC_NAME_CONSTRAIN`.
+    fn callee_is_constrain(&self) -> bool {
+        unsafe { llzkCallOpGetCalleeIsConstrain(self.to_raw()) }
+    }
+
+    /// Return `true` iff the callee function name is `LLZK_FUNC_NAME_COMPUTE` within a StructDefOp.
+    fn callee_is_struct_compute(&self) -> bool {
+        unsafe { llzkCallOpGetCalleeIsStructCompute(self.to_raw()) }
+    }
+
+    /// Return `true` iff the callee function name is `LLZK_FUNC_NAME_CONSTRAIN` within a StructDefOp.
+    fn callee_is_struct_constrain(&self) -> bool {
+        unsafe { llzkCallOpGetCalleeIsStructConstrain(self.to_raw()) }
+    }
+
+    /// Returns the "self" value (i.e. the return value) from the callee
+    /// function (which must be named `LLZK_FUNC_NAME_COMPUTE`).
+    fn self_value_of_compute(&self) -> Value<'c, 'a> {
+        unsafe { Value::from_raw(llzkCallOpGetSelfValueFromCompute(self.to_raw())) }
+    }
+
+    /// Return the "self" value (i.e. the first parameter) from the callee
+    /// function (which must be named `LLZK_FUNC_NAME_CONSTRAIN`).
+    fn self_value_of_constrain(&self) -> Value<'c, 'a> {
+        unsafe { Value::from_raw(llzkCallOpGetSelfValueFromConstrain(self.to_raw())) }
+    }
+}
 
 //===----------------------------------------------------------------------===//
 // CallOp, CallOpRef, CallOpRefMut
--- llzk/tests/function_dialect.rs
+++ llzk/tests/function_dialect.rs
@@ -1,4 +1,4 @@
-use llzk::builder::OpBuilder;
+use llzk::builder::{OpBuilder, OpBuilderLike};
 use llzk::prelude::*;
 use melior::ir::{Location, r#type::FunctionType};
 
@@ -85,4 +85,123 @@ fn function_call() {
   function.return %0 : !felt.type
 }";
     assert_eq!(ir, expected);
+}
+
+fn make_empty_struct<'c>(context: &'c LlzkContext, name: &str) -> StructDefOp<'c> {
+    let loc = Location::unknown(&context);
+    let typ = StructType::from_str(&context, name);
+    r#struct::def(loc, name, &[], {
+        [
+            r#struct::helpers::compute_fn(loc, typ, &[], None).map(Into::into),
+            r#struct::helpers::constrain_fn(loc, typ, &[], None).map(Into::into),
+        ]
+    })
+    .unwrap()
+}
+
+#[test]
+fn func_def_op_self_value_of_compute() {
+    common::setup();
+    let context = LlzkContext::new();
+    let module = llzk_module(Location::unknown(&context));
+    let module_body = module.body();
+
+    let s = make_empty_struct(&context, "StructA");
+    let s = StructDefOpRef::try_from(module_body.append_operation(s.into())).unwrap();
+    assert!(s.verify());
+    log::info!("Struct passed verification");
+
+    let self_val = s.get_compute_func().unwrap().self_value_of_compute();
+    similar_asserts::assert_eq!(
+        format!("{}", self_val),
+        "%self = struct.new : <@StructA<[]>>"
+    );
+}
+
+#[test]
+fn func_def_op_self_value_of_constrain() {
+    common::setup();
+    let context = LlzkContext::new();
+    let module = llzk_module(Location::unknown(&context));
+    let module_body = module.body();
+
+    let s = make_empty_struct(&context, "StructA");
+    let s = StructDefOpRef::try_from(module_body.append_operation(s.into())).unwrap();
+    assert!(s.verify());
+    log::info!("Struct passed verification");
+
+    let self_val = s.get_constrain_func().unwrap().self_value_of_constrain();
+    similar_asserts::assert_eq!(
+        format!("{}", self_val),
+        "<block argument> of type '!struct.type<@StructA<[]>>' at index: 0"
+    );
+}
+
+#[test]
+fn call_op_self_value_of_compute() {
+    common::setup();
+    let context = LlzkContext::new();
+    let module = llzk_module(Location::unknown(&context));
+    let module_body = module.body();
+
+    let s1 = make_empty_struct(&context, "StructA");
+    let s1 = StructDefOpRef::try_from(module_body.append_operation(s1.into())).unwrap();
+    assert!(s1.verify());
+    log::info!("Struct 1 passed verification");
+
+    let s2 = make_empty_struct(&context, "StructB");
+    let s2 = StructDefOpRef::try_from(module_body.append_operation(s2.into())).unwrap();
+    assert!(s2.verify());
+    log::info!("Struct 2 passed verification");
+
+    let s2_compute_body = s2
+        .get_compute_func()
+        .unwrap()
+        .region(0)
+        .unwrap()
+        .first_block()
+        .unwrap();
+    let builder = OpBuilder::at_block_begin(&context, s2_compute_body);
+    let loc = Location::unknown(&context);
+    let call = builder.insert(loc, |_, loc| {
+        let name = SymbolRefAttribute::new(&context, "StructA", &["compute"]);
+        function::call(&builder, loc, name, &[], &[s1.r#type()])
+            .unwrap()
+            .into()
+    });
+
+    // First ensure it's properly formed
+    let ir = format!("{}", module.as_operation());
+    let expected = r#"module attributes {veridise.lang = "llzk"} {
+  struct.def @StructA<[]> {
+    function.def @compute() -> !struct.type<@StructA<[]>> attributes {function.allow_witness} {
+      %self = struct.new : <@StructA<[]>>
+      function.return %self : !struct.type<@StructA<[]>>
+    }
+    function.def @constrain(%arg0: !struct.type<@StructA<[]>>) attributes {function.allow_constraint} {
+      function.return
+    }
+  }
+  struct.def @StructB<[]> {
+    function.def @compute() -> !struct.type<@StructB<[]>> attributes {function.allow_witness} {
+      %self = struct.new : <@StructB<[]>>
+      %0 = function.call @StructA::@compute() : () -> !struct.type<@StructA<[]>> 
+      function.return %self : !struct.type<@StructB<[]>>
+    }
+    function.def @constrain(%arg0: !struct.type<@StructB<[]>>) attributes {function.allow_constraint} {
+      function.return
+    }
+  }
+}
+"#;
+    similar_asserts::assert_eq!(ir, expected);
+
+    // Now actually test the `self_value_of_compute` function
+    let call = CallOpRef::try_from(call).unwrap();
+    let self_val = call.self_value_of_compute();
+    similar_asserts::assert_eq!(
+        format!("{}", self_val),
+        // Yes, the line does have a trailing space, here and in the entire IR above.
+        "%0 = function.call @StructA::@compute() : () -> !struct.type<@StructA<[]>> "
+    );
 }
