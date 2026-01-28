dcfd4069d3898a957e78b6d3fa235d6bd8afc292
foldr <daniel@veridise.com>
2026-01-28 19:23:24 +0100

    Make ArrayType implement Copy and Clone (#92)

--- llzk/src/dialect/array/type.rs
+++ llzk/src/dialect/array/type.rs
@@ -9,7 +9,7 @@ use melior::ir::{Attribute, Type, TypeLike};
 use mlir_sys::MlirType;
 
 /// Represents the `!array.type` type.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct ArrayType<'c> {
     r#type: Type<'c>,
 }
